76eada1536e432a0bd95d6acb7ee9a98953b63e0
SMT016 <SMT016@DESKTOP-I4ER4CU>
2021-02-17 15:01:25 +0900

    미세먼지, 자외선 api 프로젝트에 합본 실시 (추후 기상페이지에 적용예정) - 장요셉

---
+++ src/com/controller/UV.java
@@ -0,0 +1,44 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
+
+import com.model.weatherDAO;
+
+
+@WebServlet("/UV")
+public class UV extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   
+      HttpSession session = request.getSession();
+      
+      weatherDAO weatherdao = new weatherDAO();
+      
+      ArrayList<String> uvlist =   weatherdao.uvtoday();
+      
+      PrintWriter out = response.getWriter();
+      JSONObject test = new JSONObject();
+      test.put("weather", uvlist.get(1));
+      out.print(test);
+      
+      System.out.println("uvlist: " + uvlist);
+      
+      session.setAttribute("uv", uvlist);
+      
+      response.sendRedirect("aaa.jsp");
+   }
+
+}
